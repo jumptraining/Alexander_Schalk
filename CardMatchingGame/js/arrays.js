bbe9c0bfe7c260b4a4545315a732e26ce47c3ee8
Alexander Schalk <Alexanderlschalk@gmail.com>
2020-09-24 04:14:14 -0500

    add project files

---
+++ CardMatchingGame/js/arrays.js
@@ -0,0 +1,255 @@
+// Clubs card data
+const cardsArrayC = [
+    {
+        name: '2 of clubs',
+        facevalue: 'two',
+        img: 'cards_png/2C.png'
+  },
+    {
+        name: '3 of clubs',
+        facevalue: 'three',
+        img: 'cards_png/3C.png'
+  },
+    {
+        name: '4 of clubs',
+        facevalue: 'four',
+        img: 'cards_png/4C.png'
+  },
+    {
+        name: '5 of clubs',
+        facevalue: 'five',
+        img: 'cards_png/5C.png'
+  },
+    {
+        name: '6 of clubs',
+        facevalue: 'six',
+        img: 'cards_png/6C.png'
+  },
+    {
+        name: '7 of clubs',
+        facevalue: 'seven',
+        img: 'cards_png/7C.png'
+  },
+    {
+        name: '8 of clubs',
+        facevalue: 'eight',
+        img: 'cards_png/8C.png'
+  },
+    {
+        name: '9 of clubs',
+        facevalue: 'nine',
+        img: 'cards_png/9C.png'
+  },
+    {
+        name: '10 of clubs',
+        facevalue: 'ten',
+        img: 'cards_png/10C.png'
+  },
+    {
+        name: 'jack of clubs',
+        facevalue: 'jack',
+        img: 'cards_png/JC.png'
+  },
+    {
+        name: 'queen of clubs',
+        facevalue: 'queen',
+        img: 'cards_png/QC.png'
+  },
+    {
+        name: 'king of clubs',
+        facevalue: 'king',
+        img: 'cards_png/KC.png'
+  }
+]
+
+// Spades card data
+const cardsArrayS = [
+    {
+        name: '2 of spades',
+        facevalue: 'two',
+        img: 'cards_png/2S.png'
+  },
+    {
+        name: '3 of spades',
+        facevalue: 'three',
+        img: 'cards_png/3S.png'
+  },
+    {
+        name: '4 of spades',
+        facevalue: 'four',
+        img: 'cards_png/4S.png'
+  },
+    {
+        name: '5 of spades',
+        facevalue: 'five',
+        img: 'cards_png/5S.png'
+  },
+    {
+        name: '6 of spades',
+        facevalue: 'six',
+        img: 'cards_png/6S.png'
+  },
+    {
+        name: '7 of spades',
+        facevalue: 'seven',
+        img: 'cards_png/7S.png'
+  },
+    {
+        name: '8 of spades',
+        facevalue: 'eight',
+        img: 'cards_png/8S.png'
+  },
+    {
+        name: '9 of spades',
+        facevalue: 'nine',
+        img: 'cards_png/9S.png'
+  },
+    {
+        name: '10 of spades',
+        facevalue: 'ten',
+        img: 'cards_png/10S.png'
+  },
+    {
+        name: 'jack of spades',
+        facevalue: 'jack',
+        img: 'cards_png/JS.png'
+  },
+    {
+        name: 'queen of spades',
+        facevalue: 'queen',
+        img: 'cards_png/QS.png'
+  },
+    {
+        name: 'king of spades',
+        facevalue: 'king',
+        img: 'cards_png/KS.png'
+  }
+]
+
+// Hearts card data
+const cardsArrayH = [
+    {
+        name: '2 of hearts',
+        facevalue: 'two',
+        img: 'cards_png/2H.png'
+  },
+    {
+        name: '3 of hearts',
+        facevalue: 'three',
+        img: 'cards_png/3H.png'
+  },
+    {
+        name: '4 of hearts',
+        facevalue: 'four',
+        img: 'cards_png/4H.png'
+  },
+    {
+        name: '5 of hearts',
+        facevalue: 'five',
+        img: 'cards_png/5H.png'
+  },
+    {
+        name: '6 of hearts',
+        facevalue: 'six',
+        img: 'cards_png/6H.png'
+  },
+    {
+        name: '7 of hearts',
+        facevalue: 'seven',
+        img: 'cards_png/7H.png'
+  },
+    {
+        name: '8 of hearts',
+        facevalue: 'eight',
+        img: 'cards_png/8H.png'
+  },
+    {
+        name: '9 of hearts',
+        facevalue: 'nine',
+        img: 'cards_png/9H.png'
+  },
+    {
+        name: '10 of hearts',
+        facevalue: 'ten',
+        img: 'cards_png/10H.png'
+  },
+    {
+        name: 'jack of hearts',
+        facevalue: 'jack',
+        img: 'cards_png/JH.png'
+  },
+    {
+        name: 'queen of hearts',
+        facevalue: 'queen',
+        img: 'cards_png/QH.png'
+  },
+    {
+        name: 'king of hearts',
+        facevalue: 'king',
+        img: 'cards_png/KH.png'
+  }
+]
+
+// Diamonds card data
+const cardsArrayD = [
+    {
+        name: '2 of diamonds',
+        facevalue: 'two',
+        img: 'cards_png/2D.png'
+  },
+    {
+        name: '3 of diamonds',
+        facevalue: 'three',
+        img: 'cards_png/3D.png'
+  },
+    {
+        name: '4 of diamonds',
+        facevalue: 'four',
+        img: 'cards_png/4D.png'
+  },
+    {
+        name: '5 of diamonds',
+        facevalue: 'five',
+        img: 'cards_png/5D.png'
+  },
+    {
+        name: '6 of diamonds',
+        facevalue: 'six',
+        img: 'cards_png/6D.png'
+  },
+    {
+        name: '7 of diamonds',
+        facevalue: 'seven',
+        img: 'cards_png/7D.png'
+  },
+    {
+        name: '8 of diamonds',
+        facevalue: 'eight',
+        img: 'cards_png/8D.png'
+  },
+    {
+        name: '9 of diamonds',
+        facevalue: 'nine',
+        img: 'cards_png/9D.png'
+  },
+    {
+        name: '10 of diamonds',
+        facevalue: 'ten',
+        img: 'cards_png/10D.png'
+  },
+    {
+        name: 'jack of diamonds',
+        facevalue: 'jack',
+        img: 'cards_png/JD.png'
+  },
+    {
+        name: 'queen of diamonds',
+        facevalue: 'queen',
+        img: 'cards_png/QD.png'
+  },
+    {
+        name: 'king of diamonds',
+        facevalue: 'king',
+        img: 'cards_png/KD.png'
+  }
+]
---
+++ CardMatchingGame/js/script.js
@@ -0,0 +1,419 @@
+// Clubs card data
+const cardsArrayC = [
+    {
+        name: '2 of clubs',
+        facevalue: 'two',
+        img: 'cards_png/2C.png'
+  },
+    {
+        name: '3 of clubs',
+        facevalue: 'three',
+        img: 'cards_png/3C.png'
+  },
+    {
+        name: '4 of clubs',
+        facevalue: 'four',
+        img: 'cards_png/4C.png'
+  },
+    {
+        name: '5 of clubs',
+        facevalue: 'five',
+        img: 'cards_png/5C.png'
+  },
+    {
+        name: '6 of clubs',
+        facevalue: 'six',
+        img: 'cards_png/6C.png'
+  },
+    {
+        name: '7 of clubs',
+        facevalue: 'seven',
+        img: 'cards_png/7C.png'
+  },
+    {
+        name: '8 of clubs',
+        facevalue: 'eight',
+        img: 'cards_png/8C.png'
+  },
+    {
+        name: '9 of clubs',
+        facevalue: 'nine',
+        img: 'cards_png/9C.png'
+  },
+    {
+        name: '10 of clubs',
+        facevalue: 'ten',
+        img: 'cards_png/10C.png'
+  },
+    {
+        name: 'jack of clubs',
+        facevalue: 'jack',
+        img: 'cards_png/JC.png'
+  },
+    {
+        name: 'queen of clubs',
+        facevalue: 'queen',
+        img: 'cards_png/QC.png'
+  },
+    {
+        name: 'king of clubs',
+        facevalue: 'king',
+        img: 'cards_png/KC.png'
+  }
+]
+
+// Spades card data
+const cardsArrayS = [
+    {
+        name: '2 of spades',
+        facevalue: 'two',
+        img: 'cards_png/2S.png'
+  },
+    {
+        name: '3 of spades',
+        facevalue: 'three',
+        img: 'cards_png/3S.png'
+  },
+    {
+        name: '4 of spades',
+        facevalue: 'four',
+        img: 'cards_png/4S.png'
+  },
+    {
+        name: '5 of spades',
+        facevalue: 'five',
+        img: 'cards_png/5S.png'
+  },
+    {
+        name: '6 of spades',
+        facevalue: 'six',
+        img: 'cards_png/6S.png'
+  },
+    {
+        name: '7 of spades',
+        facevalue: 'seven',
+        img: 'cards_png/7S.png'
+  },
+    {
+        name: '8 of spades',
+        facevalue: 'eight',
+        img: 'cards_png/8S.png'
+  },
+    {
+        name: '9 of spades',
+        facevalue: 'nine',
+        img: 'cards_png/9S.png'
+  },
+    {
+        name: '10 of spades',
+        facevalue: 'ten',
+        img: 'cards_png/10S.png'
+  },
+    {
+        name: 'jack of spades',
+        facevalue: 'jack',
+        img: 'cards_png/JS.png'
+  },
+    {
+        name: 'queen of spades',
+        facevalue: 'queen',
+        img: 'cards_png/QS.png'
+  },
+    {
+        name: 'king of spades',
+        facevalue: 'king',
+        img: 'cards_png/KS.png'
+  }
+]
+
+// Hearts card data
+const cardsArrayH = [
+    {
+        name: '2 of hearts',
+        facevalue: 'two',
+        img: 'cards_png/2H.png'
+  },
+    {
+        name: '3 of hearts',
+        facevalue: 'three',
+        img: 'cards_png/3H.png'
+  },
+    {
+        name: '4 of hearts',
+        facevalue: 'four',
+        img: 'cards_png/4H.png'
+  },
+    {
+        name: '5 of hearts',
+        facevalue: 'five',
+        img: 'cards_png/5H.png'
+  },
+    {
+        name: '6 of hearts',
+        facevalue: 'six',
+        img: 'cards_png/6H.png'
+  },
+    {
+        name: '7 of hearts',
+        facevalue: 'seven',
+        img: 'cards_png/7H.png'
+  },
+    {
+        name: '8 of hearts',
+        facevalue: 'eight',
+        img: 'cards_png/8H.png'
+  },
+    {
+        name: '9 of hearts',
+        facevalue: 'nine',
+        img: 'cards_png/9H.png'
+  },
+    {
+        name: '10 of hearts',
+        facevalue: 'ten',
+        img: 'cards_png/10H.png'
+  },
+    {
+        name: 'jack of hearts',
+        facevalue: 'jack',
+        img: 'cards_png/JH.png'
+  },
+    {
+        name: 'queen of hearts',
+        facevalue: 'queen',
+        img: 'cards_png/QH.png'
+  },
+    {
+        name: 'king of hearts',
+        facevalue: 'king',
+        img: 'cards_png/KH.png'
+  }
+]
+
+// Diamonds card data
+const cardsArrayD = [
+    {
+        name: '2 of diamonds',
+        facevalue: 'two',
+        img: 'cards_png/2D.png'
+  },
+    {
+        name: '3 of diamonds',
+        facevalue: 'three',
+        img: 'cards_png/3D.png'
+  },
+    {
+        name: '4 of diamonds',
+        facevalue: 'four',
+        img: 'cards_png/4D.png'
+  },
+    {
+        name: '5 of diamonds',
+        facevalue: 'five',
+        img: 'cards_png/5D.png'
+  },
+    {
+        name: '6 of diamonds',
+        facevalue: 'six',
+        img: 'cards_png/6D.png'
+  },
+    {
+        name: '7 of diamonds',
+        facevalue: 'seven',
+        img: 'cards_png/7D.png'
+  },
+    {
+        name: '8 of diamonds',
+        facevalue: 'eight',
+        img: 'cards_png/8D.png'
+  },
+    {
+        name: '9 of diamonds',
+        facevalue: 'nine',
+        img: 'cards_png/9D.png'
+  },
+    {
+        name: '10 of diamonds',
+        facevalue: 'ten',
+        img: 'cards_png/10D.png'
+  },
+    {
+        name: 'jack of diamonds',
+        facevalue: 'jack',
+        img: 'cards_png/JD.png'
+  },
+    {
+        name: 'queen of diamonds',
+        facevalue: 'queen',
+        img: 'cards_png/QD.png'
+  },
+    {
+        name: 'king of diamonds',
+        facevalue: 'king',
+        img: 'cards_png/KD.png'
+  }
+]
+
+//*************************//
+
+// Duplicate array to create a match for each card
+// color of suits is randomly chosen
+var suit = Math.round(Math.random())
+let gameGrid, frontcolor
+suit == 0 ?
+    (gameGrid = (cardsArrayC.concat(cardsArrayS)), console.log('Match the black suits!'), frontcolor = `url('cards_png/gray_back.png')`) :
+    (gameGrid = cardsArrayH.concat(cardsArrayD), console.log('Match the red suits!'), frontcolor = `url('cards_png/red_back.png')`)
+
+// NOTE: add code that changes the wording Match the black/red suits
+
+// Randomize game grid on each load
+gameGrid.sort(() => 0.5 - Math.random())
+
+let firstGuess = ''
+let secondGuess = ''
+let count = 0,
+    matches = 0
+let previousTarget = null
+let delay = 1000
+
+//*************************//
+
+// Grab the div with an id of root
+const game = document.getElementById('game')
+
+// Create a section with a class of grid
+const grid = document.createElement('section')
+grid.setAttribute('class', 'grid')
+
+// Append the grid section to the game div
+game.appendChild(grid)
+
+//*************************//
+
+// For each item in the cardsArray array...
+gameGrid.forEach(item => {
+    // Create card element with the name dataset
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.dataset.name = item.name
+
+    // Create front of card
+    const front = document.createElement('div') //moved const
+    front.classList.add('front')
+    front.style.backgroundImage = frontcolor
+
+    // Apply a card class to that div
+    card.classList.add('card')
+
+    // Set the data-name attribute of the div to the cardsArray name
+    card.dataset.name = item.name
+    card.dataset.facevalue = item.facevalue //added line
+
+    // Create back of card, which contains
+    const back = document.createElement('div')
+    back.classList.add('back')
+    back.style.backgroundImage = `url(${item.img})`
+
+    // Append card to grid, and front and back to each card
+    grid.appendChild(card)
+    card.appendChild(front)
+    card.appendChild(back)
+})
+
+//*************************//
+
+// Add match CSS
+const match = () => {
+    var selected = document.querySelectorAll('.selected')
+    selected.forEach(card => {
+        card.classList.add('match')
+
+        console.log(card) //card[0].style.backgroundImage = `url('cards_png/green_back.png')`
+//        card.font.style.background = `url('cards_png/green_back.png')`; //added
+
+    })
+
+    console.log('A match was found!')
+
+    matches++
+    if (matches === 12) {
+        //something to indicate player won
+        document.getElementById('message').innerHTML =
+            '<h1>' +
+            'Congratulations, you won!' +
+            '</h1>'
+    }
+}
+
+//*************************//
+
+const resetGuesses = () => {
+    firstGuess = ''
+    secondGuess = ''
+    count = 0
+    previousTarget = null
+
+    var selected = document.querySelectorAll('.selected')
+    selected.forEach(card => {
+        card.classList.remove('selected')
+    })
+
+    console.log('') //blank line
+
+}
+
+//*************************//
+
+
+function playSound1() {
+    var audio1 = new Audio('sounds/flip.wav');
+    audio1.play();
+}
+
+function playSound2() {
+    var audio2 = new Audio('sounds/matched.wav');
+    audio2.play();
+}
+
+// Add event listener to grid
+grid.addEventListener('click', event => {
+
+    playSound1() //added
+
+    const clicked = event.target
+
+    if (
+        clicked.nodeName === 'SECTION' ||
+        clicked === previousTarget ||
+        clicked.parentNode.classList.contains('selected') ||
+        clicked.parentNode.classList.contains('match')
+    ) {
+        return
+    }
+
+    if (count < 2) {
+        count++
+        if (count === 1) {
+            // Assign first guess
+            firstGuess = clicked.parentNode.dataset.facevalue
+            console.log('The ' + clicked.parentNode.dataset.name + ' was selected.')
+            clicked.parentNode.classList.add('selected')
+        } else {
+            // Assign second guess
+            secondGuess = clicked.parentNode.dataset.facevalue
+            console.log('The ' + clicked.parentNode.dataset.name + ' was selected.')
+            clicked.parentNode.classList.add('selected')
+        }
+        // If both guesses are not empty...
+        if (firstGuess !== '' && secondGuess !== '') {
+            // and the first guess matches the second match...
+            if (firstGuess === secondGuess) {
+                playSound2()
+                setTimeout(match, delay)
+                setTimeout(resetGuesses, delay)
+            } else {
+                setTimeout(resetGuesses, delay)
+            }
+        }
+    }
+})
